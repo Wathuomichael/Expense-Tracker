17be581b2eff9ce8220a409e7fca872bafc76485
Wathuomichael <wathuomichael@gmail.com>
2024-03-29 23:26:12 +0300

    GET fix

--- app/api/route.ts
+++ app/api/route.ts
@@ -2,7 +2,7 @@ import { conn } from "@/utils/db";
 import { userModel } from "@/models/userModel"; 
 import { NextResponse } from "next/server";
 
-export async function GET(res: Response) {
+export async function GET(req: Request, res: Response) {
     await conn();
 
     const info = await userModel.find();
